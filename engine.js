32a5ca1460ceabf2ceb18f99724fed4d2da7b9fe
LuisGaxiola117 <luisgaxiola117@gmail.com>
2024-06-24 08:39:55 -0600

    funciones

--- engine.js
+++ engine.js
@@ -1,21 +1,32 @@
 
+function moverElemento(checkbox) {
+    // Obtén el elemento padre (li) del checkbox
+    const listItem = checkbox.parentElement;
+    
+    // Crea una nueva lista y mueve el elemento a ella
+    const otraLista = document.getElementById("otraLista");
+    otraLista.appendChild(listItem);
+}
+
 function agregarElemento() {
     event.preventDefault();
-    var input = document.getElementById("input").value;
-    
+    const input = document.getElementById("input").value;
 
-    var li = document.createElement("li");
+    const li = document.createElement("li");
     li.textContent = input;
 
-    var checkbox = document.createElement("input");
+    const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    
+    checkbox.onclick = function() {
+        moverElemento(this);
+    };
 
     li.appendChild(checkbox);
 
-    var miLista = document.getElementById("miLista").querySelector("ul");
+    const miLista = document.getElementById("miLista").querySelector("ul");
     miLista.appendChild(li);
-    
 
     document.getElementById("input").value = "";
 }
+
+
